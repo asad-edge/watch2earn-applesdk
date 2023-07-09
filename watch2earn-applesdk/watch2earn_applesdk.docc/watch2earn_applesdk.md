89e42eac214a8bdbd88990871fb7d267d8c04a78
asad926 <asad@edgevideo.com>
2023-07-09 09:56:18 +0500

    watch2earn sdk update

---
+++ watch2earn-applesdk/watch2earn_applesdk.docc/watch2earn_applesdk.md
@@ -0,0 +1,13 @@
+# ``watch2earn_applesdk``
+
+<!--@START_MENU_TOKEN@-->Summary<!--@END_MENU_TOKEN@-->
+
+## Overview
+
+<!--@START_MENU_TOKEN@-->Text<!--@END_MENU_TOKEN@-->
+
+## Topics
+
+### <!--@START_MENU_TOKEN@-->Group<!--@END_MENU_TOKEN@-->
+
+- <!--@START_MENU_TOKEN@-->``Symbol``<!--@END_MENU_TOKEN@-->
---
+++ watch2earn-applesdk/watch2earn_applesdk.h
@@ -0,0 +1,16 @@
+//
+//  watch2earn_applesdk.h
+//  watch2earn-applesdk
+//
+//  Created by Asad Iqbal on 13/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for watch2earn_applesdk.
+FOUNDATION_EXPORT double watch2earn_applesdkVersionNumber;
+
+//! Project version string for watch2earn_applesdk.
+FOUNDATION_EXPORT const unsigned char watch2earn_applesdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <watch2earn_applesdk/PublicHeader.h>
